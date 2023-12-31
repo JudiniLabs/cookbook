531ff655180fbcca34c7d120399b95bd19b4e0eb
Dani <dan.avila7@gmail.com>
2023-12-31 17:34:33 -0500

    streamlit simple chat agent

---
+++ API/streamlit_simple_chat/app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import time
+import os
+from judini.codegpt.chat import Completion
+from judini.codegpt.document import Document
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key= os.getenv("CODEGPT_API_KEY")
+agent_id= os.getenv("CODEGPT_AGENT_ID")
+st.set_page_config(layout="centered")
+
+st.title("Simple Streamlit Chat CodeGPT Agent 🤖")
+st.markdown('---')
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("En que te puedo ayudar?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        with st.spinner('Wait for it...'):
+            message_placeholder = st.empty()
+            full_response = ""
+
+            completion = Completion(api_key)
+            prompt = st.session_state.messages
+
+            response_completion = completion.create(agent_id, prompt, stream=False)
+            for response in response_completion:
+                time.sleep(0.05)
+                full_response += (response or "")
+                message_placeholder.markdown(full_response + "▌")       
+            message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    
