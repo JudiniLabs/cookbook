f6d880eedda66778a3572fa7bd3d30c54e765aff
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2024-01-09 12:58:27 -0500

    fixed: agent selector

--- examples/whatsapp_chatbot/frontend/whatsapp-bot-frontend/src/MainInterface.js
+++ examples/whatsapp_chatbot/frontend/whatsapp-bot-frontend/src/MainInterface.js
@@ -2,31 +2,32 @@ import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import './mainInterface.css';
 
-const socket = io('http://localhost:3001'); // Update with your actual socket server URL
+const socket = io('http://localhost:3001');
 
 const MainInterface = ({ onShowModalChange }) => {
   const [number, setNumber] = useState(0);
   const [apiKey, setApiKey] = useState('');
   const [agent, setAgent] = useState('No tienes agente por defecto');
   const [editMode, setEditMode] = useState({ apiKey: false, agent: false });
-  const [agents, setAgents] = useState([]); // New state for the list of agents
+  const [agents, setAgents] = useState([]);
+  const [apiKeyReceived, setApiKeyReceived] = useState(false); // Nuevo estado
 
   useEffect(() => {
-    // Configurar el listener para recibir el estado del socket
     socket.on('socketData', (data) => {
       console.log(data)
       setNumber(data.number);
-      setApiKey(data.apiKey);
+      if (data.apiKey) { // Si la apiKey ha sido recibida por el socket
+        setApiKey(data.apiKey);
+        setApiKeyReceived(true); // Actualiza el estado
+      }
       setAgent(data.agent || 'No tienes agente por defecto');
     });
 
-    // Nuevo listener para el evento 'qr'
     socket.on('qr', (data) => {
       console.log("data",data)
       onShowModalChange(data);
     });
 
-    // Limpiar los listeners cuando el componente se desmonta
     return () => {
       socket.off('socketData');
       socket.off('qr');
@@ -35,7 +36,6 @@ const MainInterface = ({ onShowModalChange }) => {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    // Solo enviar el apiKey y el agente al socket si tienen un valor distinto de '' o 'No tienes agente por defecto'
     const dataToSend = {};
     if (apiKey !== '') {
       dataToSend.apiKey = apiKey;
@@ -52,7 +52,6 @@ const MainInterface = ({ onShowModalChange }) => {
   const handleEditClick = (field) => {
     setEditMode({ ...editMode, [field]: true });
     if (field === 'agent') {
-      // Request the list of agents from the backend when the user clicks edit
       socket.emit('requestAgents');
     }
   };
@@ -61,7 +60,6 @@ const MainInterface = ({ onShowModalChange }) => {
     setEditMode({ ...editMode, [field]: false });
   };
 
-  // New useEffect to listen for the 'agents' event from the socket
   useEffect(() => {
     socket.on('agents', (data) => {
       setAgents(data);
@@ -78,7 +76,7 @@ const MainInterface = ({ onShowModalChange }) => {
       <form onSubmit={handleFormSubmit} className="data-form">
         <div className="data-field">
           <label>Número Enlazado:</label>
-          <span>{number}</span> {/* El número enlazado ya no es editable */}
+          <span>{number}</span>
         </div>
         <div className="data-field">
           <label>codeGPT API Key:</label>
@@ -91,21 +89,23 @@ const MainInterface = ({ onShowModalChange }) => {
             <span>{apiKey} <button onClick={() => handleEditClick('apiKey')}>Editar</button></span>
           )}
         </div>
-        <div className="data-field">
-          <label>Agente conectado actualmente:</label>
-          {editMode.agent ? (
-            <>
-              <select value={agent} onChange={(e) => setAgent(e.target.value)}>
-                {agents.map((agent) => (
-                  <option key={agent} value={agent}>{agent}</option>
-                ))}
-              </select>
-              <button onClick={() => handleCancelClick('agent')}>X</button>
-            </>
-          ) : (
-            <span>{agent} <button onClick={() => handleEditClick('agent')}>Editar</button></span>
-          )}
-        </div>
+        {apiKeyReceived && (
+          <div className="data-field">
+            <label>Agente conectado actualmente:</label>
+            {editMode.agent ? (
+              <>
+                <select value={agent} onChange={(e) => setAgent(e.target.value)}>
+                  {agents.map((agent) => (
+                    <option key={agent} value={agent}>{agent}</option>
+                  ))}
+                </select>
+                <button onClick={() => handleCancelClick('agent')}>X</button>
+              </>
+            ) : (
+              <span>{agent} <button onClick={() => handleEditClick('agent')}>Editar</button></span>
+            )}
+          </div>
+        )}
         <button type="submit">Actualizar Datos</button>
       </form>
     </div>
--- examples/whatsapp_chatbot/src/sockets/socketControllers/getAgents.js
+++ examples/whatsapp_chatbot/src/sockets/socketControllers/getAgents.js
@@ -8,18 +8,22 @@ let codeGPTApi;
 function getCodeGPTApi() {
   if (!codeGPTApi) {
     const apiKey = instanciasBot[process.env.NAME_CHATBOT].apiKey;
+    console.log(apiKey)
     codeGPTApi = new CodeGPTApi(process.env.GENERAL_URL_API, apiKey);
   }
   return codeGPTApi;
 }
 const getAgents = async () => {
+  try {
     let response = await getCodeGPTApi().listAgents();
-    
-  
+    console.log("Response", response)
     let agentIds = response.map(agent => agent.name);
    
-   return agentIds
+    return agentIds
+  } catch (error) {
+    console.error(error);
   }
+}
 
  
 module.exports  = {
